fa2ff9f899a10b981dd1659bc0577e5cb9b5a500
ekonuma <konumaeike5618@gmail.com>
2024-01-18 11:02:00 -0300

    feat(finish abc085c)

--- atcoder/beginner/abc085c/main.go
+++ atcoder/beginner/abc085c/main.go
@@ -3,42 +3,20 @@ package main
 import "fmt"
 
 func main()  {
-	var n,y, sen, gosen, itiman, total int
+	var n, y int
 	fmt.Scan(&n)
 	fmt.Scan(&y)
+	fmt.Println(otoshidama(n, y))
+}
 
-	(10 * i) +  (5 * g) + s = y
-	total = y
-
-	for(y % 1000 == 0) {
-		y -= y/1000 
-		sen+= y/1000
-	 }
-	 if sen > n{
-		y = total
-		for(y % 5000 == 0) {
-			y -=  y/5000 
-			gosen+= y/5000
-		 } 
-		 if gosen > n{
-			y = total
-			for(y %10000 == 0) {
-				y -=  y/10000
-				itiman+= y/10000
-			 } 
-			 for(y % 5000 == 0) {
-				y -=  y/5000
-				gosen+= y/5000
-			 }  
-			 for(y % 1000 == 0) {
-				y -=  y/1000
-				sen+= y/1000
-			 }  
-		 }
-	 }
-	 for(y % 5000 == 0) {
-		y -=  y/5000
-		gosen+= y/5000
-	 }
-	 fmt.Println(itiman, gosen, sen)
+func otoshidama(n int, y int) string{
+	for itiman := 0; itiman <= n; itiman++{
+		for gosen := 0; gosen <= n-itiman; gosen++{
+			sen := n - itiman - gosen
+			if itiman * 10000 + gosen * 5000 + sen * 1000 == y{
+				return fmt.Sprintln(itiman, gosen, sen)
+			}
+		}	
+	}
+	return fmt.Sprintln("-1", "-1", "-1") 
 }
